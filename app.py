d98c5499e069f1077a0d864e4f8b487e890a01eb
jondkelley <jonk@omg.lol>
2021-04-02 10:41:44 -0500

    Add logging to example render app

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import logging
 app = Flask(__name__)
 
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+log.debug('Startup Info message')
+log.info('Startup Info message')
+log.warning('Startup Warning message')
+log.error('Startup Error message')
 @app.route('/')
 def hello_world():
+    log.info('Info message')
+    log.warning('Warning message')
+    log.error('Error message')
     return 'Hello, World!'
